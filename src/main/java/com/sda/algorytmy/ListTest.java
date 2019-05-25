ef268f2ac9e5007a27dce18a56f1eadd5a6e4d24
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-25 15:57:06 +0100

    commit

--- src/main/java/com/sda/algorytmy/ListTest.java
+++ src/main/java/com/sda/algorytmy/ListTest.java
@@ -5,29 +5,15 @@ public class ListTest {
     public static void main(String[] args) {
 
         MyList<Integer> list = new ListImpl<>();
+
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
-        list.add(5);
         list.remove(2);
+
+        MyList <Integer> list1 = newLinkedListImpl ();
 
     }
 }
